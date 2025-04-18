e4c794078c695ccf3e22aac0017b532bcfb60370
Sujoy Das <me@binarycodes.io>
2025-04-18 23:57:35 +0300

    clean up unnecessary UI elements

--- ssh-key-signer-server/src/main/frontend/views/@index.tsx
+++ ssh-key-signer-server/src/main/frontend/views/@index.tsx
@@ -1,7 +1,4 @@
-
 import { ViewConfig } from '@vaadin/hilla-file-router/types.js';
-import { Button, Card, Notification, Upload, UploadElement } from '@vaadin/react-components';
-import { useEffect, useRef } from 'react';
 
 export const config: ViewConfig = {
   title: "Sign Key",
@@ -13,39 +10,12 @@ const getMyCsrfHeaders: () => Record<string, string> = () => {
   return { 'X-CSRF-Token': token };
 }
 
-const maxFileSizeInMB = 10;
-const maxFileSizeInBytes = maxFileSizeInMB * 1024 * 1024;
-
 export default function MainView() {
   const csrfHeaders = getMyCsrfHeaders();
-  const uploadRef = useRef<UploadElement>(null);
-
-  useEffect(() => {
-    if (!uploadRef.current) {
-      return;
-    }
-    uploadRef.current.i18n.addFiles.many = 'Select Files...';
-    uploadRef.current.i18n = { ...uploadRef.current.i18n };
-  }, [uploadRef.current]);
-
 
   return (
     <>
-      <Card theme="elevated">
-        <div slot="title">Upload user key</div>
-        <div slot="subtitle">Only upload the public key</div>
-        <Upload
-                target="/rest/upload/file"
-                accept=".pub"
-                maxFiles={1}
-                maxFileSize={maxFileSizeInBytes}
-                onFileReject={(event) => {
-                  Notification.show(event.detail.error);
-                }}
-                headers={csrfHeaders}
-              />
-        <Button theme="primary">Submit</Button>
-      </Card>
+      <span>Under Construction</span>
     </>
   );
 }
