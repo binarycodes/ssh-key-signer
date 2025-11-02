174dc0e03406f2c3fea36fb44446b734a093ca5e
Sujoy Das <me@binarycodes.io>
2025-11-02 16:27:03 +0200

    chore: tag creating script

---
+++ retry-tag-workflow.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+tagname="v0.0.7"
+
+git tag -d "$tagname"
+git push --delete origin "$tagname"
+
+git tag -a "$tagname" -m "release version $tagname"
+git push origin tag "$tagname"
